ec852ed080ee4506738a97d92cf60ec6af516ebc
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-19 12:41:29 +0200

    some alignments

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -8,10 +8,10 @@ const HeroCard = (props) => {
     <>
       <div className="flex justify-center">
         <Link to={`/heroes/${slug}`}>
-          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+          <div className="rounded-lg shadow-lg bg-white/80 max-w-sm">
             <img className="rounded-t-lg" src={image} alt={`${name} image`}/>
             <div className="p-6">
-              <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
+              <h5 className="text-gray-900 text-xl font-medium mb-2 capitalize">{name}</h5>
             </div>
           </div>
         </Link>
