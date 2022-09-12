52f118c4e0b96765d4aecf3b7a00af1d57670ac5
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-12 12:37:23 +0200

    details

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,45 +1,70 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const [clicked, setClicked] = useState(false)
+  const [clicked, setClicked] = useState(false);
 
   const handleClickBurger = () => {
-    if(clicked){
-      setClicked(false)
-    }else{
-      setClicked(true)
+    if (clicked) {
+      setClicked(false);
+    } else {
+      setClicked(true);
     }
-  }
+  };
 
-  return(
+  return (
     <>
-      <nav className="p-5 bg-transparent shadow md:flex md:items-center md:justify-between text-white font-medium">
+      <nav className="p-5 bg-black shadow md:flex md:items-center md:justify-between text-white font-medium">
         <div className="flex justify-between items-center ">
-          <Link className="text-2xl font-[Poppins] cursor-pointer capitalize" to='/heroes'>
+          <Link
+            className="text-2xl font-[Poppins] cursor-pointer capitalize"
+            to="/heroes"
+          >
             Heroes
           </Link>
 
-          <button className="text-3xl cursor-pointer mx-2 md:hidden block" onClick={handleClickBurger}>
-            <ion-icon name="menu"
-            // onclick="Menu(this)"
+          <button
+            className="text-3xl cursor-pointer mx-2 md:hidden block"
+            onClick={handleClickBurger}
+          >
+            <ion-icon
+              name="menu"
+              // onclick="Menu(this)"
             ></ion-icon>
           </button>
         </div>
 
-        <ul className={`md:flex md:items-center md:z-auto md:static md:bg-transparent  w-full left-0 md:w-auto md:py-0 md:pl-0 md:opacity-100
-        ${clicked ? ('z-30 pl-7 py-4 fixed bg-gray-800/80'):('opacity-0 top-[-400px] z-[-1] absolute')}
-          transition-all ease-in duration-500 uppercase text-lg`}>
+        <ul
+          className={`md:flex md:items-center md:z-auto md:static md:bg-transparent  w-full left-0 md:w-auto md:py-0 md:pl-0 md:opacity-100
+        ${
+          clicked
+            ? "z-30 pl-7 py-4 fixed bg-gray-800/80"
+            : "opacity-0 top-[-400px] z-[-1] absolute"
+        }
+          transition-all ease-in duration-500 uppercase text-lg`}
+        >
           <li className="mx-4 my-2 md:my-0">
-            <Link to='/' className=" hover:text-cyan-500 duration-500" onClick={handleClickBurger}>HOME</Link>
+            <Link
+              to="/"
+              className=" hover:text-cyan-500 duration-500"
+              onClick={handleClickBurger}
+            >
+              HOME
+            </Link>
           </li>
           <li className="mx-4 my-2 md:my-0">
-            <Link to='/heroes/add-hero' className=" hover:text-cyan-500 duration-500" onClick={handleClickBurger}>Add Hero</Link>
+            <Link
+              to="/heroes/add-hero"
+              className=" hover:text-cyan-500 duration-500"
+              onClick={handleClickBurger}
+            >
+              Add Hero
+            </Link>
           </li>
         </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
